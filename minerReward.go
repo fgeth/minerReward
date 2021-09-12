80dce91f3bb89ef0d1327645428a141fc641194c
Jeff <jeff@fgeth.com>
2021-09-12 09:52:04 -0500

    reverting back to geth imports

--- minerReward.go
+++ minerReward.go
@@ -7,12 +7,12 @@ import (
 	"math/big"
 	"strings"
 
-	ethereum "github.com/fgeth/fgeth"
-	"github.com/fgeth/fgeth/accounts/abi"
-	"github.com/fgeth/fgeth/accounts/abi/bind"
-	"github.com/fgeth/fgeth/common"
-	"github.com/fgeth/fgeth/core/types"
-	"github.com/fgeth/fgeth/event"
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
